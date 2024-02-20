15e2d6de8a8774707c5c2dff8edcf93895d28bfd
Henry Sim <henry.sim@snode.com>
2024-02-20 11:03:57 +0200

    Initial Commit

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = () => {
+    
+
+    return (
+        <Text>List Screen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
