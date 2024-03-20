ca0c66b9d857be5236330f78d58a44a61101b065
Henry Sim <henry.sim@snode.com>
2024-03-20 16:05:43 +0200

    Making a request

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
 
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                term,
+                location: "san jose",
+                limit: 50
+            }
+        });
+        setResults(response.data.businesses);
+    }
 
     return (
         <View style={styles.background}>
             <SearchBar
                 term={term}
-                onTermChange={(newTerm) => setTerm(newTerm)} 
-                onTermSubmit={() => console.log("Term submitted")}
+                onTermChange={setTerm} 
+                onTermSubmit={searchApi}
             />
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found { results.length } results</Text>
         </View>
     )
 };
