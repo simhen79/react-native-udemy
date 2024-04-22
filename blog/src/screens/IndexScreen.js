22b986492f0ee3d240e9dc636ab6505dbbac28fc
Henry Sim <henry.sim@snode.com>
2024-04-22 12:07:36 +0200

    Deleting and Show Blog Posts

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,11 +1,10 @@
 import React, { useContext } from "react";
-import { Text, StyleSheet, FlatList, Button, View } from 'react-native';
+import { Text, StyleSheet, FlatList, Button, View, TouchableOpacity } from 'react-native';
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
-
-    const {state, addBlogPost} = useContext(Context);
+const IndexScreen = ({ navigation }) => {
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
     return (
         <>
@@ -15,10 +14,14 @@ const IndexScreen = () => {
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>{ item.title }</Text>
-                            <Feather name="trash" style={styles.icon}/>
-                        </View>
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{ item.title } - { item.id }</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather name="trash" style={styles.icon} />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     )
                 }}
             />
