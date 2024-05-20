3a7df71cbfda796ec42f08a672fd6867844ce7d2
Henry Sim <henry.sim@snode.com>
2024-05-20 12:02:39 +0200

    Extract Form logic

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,47 +1,23 @@
-import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet } from 'react-native';
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
+    const { state, updateBlogPost } = useContext(Context);
 
     const id = navigation.getParam('id');
 
-    const { state, updateBlogPost } = useContext(Context);
-
     const blogPost = state.find((blogPost) => blogPost.id === id);
 
-    const [title, setTitle] = useState(blogPost.title);
-    const [content, setContent] = useState(blogPost.content);
-
-    return (
-        <View>
-            <Text style={styles.label}>New Title</Text>
-            <TextInput value={title} onChangeText={(text) => setTitle(text)} style={styles.input}/>
-            <Text style={styles.label}>New Content</Text>
-            <TextInput value={content} onChangeText={(content) => setContent(content)} style={styles.input}/>
-            <Button title="Update Post" onPress={() => {
-                updateBlogPost(id, title, content, () => {
-                    navigation.navigate('Show', { id });
-                });                
-            }}/>
-        </View>
-    );
+    return <BlogPostForm 
+        onSubmit={(title, content) => {
+            updateBlogPost(id, title, content, () => navigation.navigate('Show', { id }));
+        }} 
+        blogPost={ blogPost }
+    />
 }
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
